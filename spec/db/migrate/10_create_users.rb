58fbab889b4ce7c959b987d6b6b72c2400584da0
joker1007 <kakyoin.hierophant@gmail.com>
2016-06-18 05:57:17 +0900

    Add column to test table definition

--- spec/db/migrate/10_create_users.rb
+++ spec/db/migrate/10_create_users.rb
@@ -2,6 +2,7 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :name
+      t.integer :age, default: 0
 
       t.timestamps null: false
     end
